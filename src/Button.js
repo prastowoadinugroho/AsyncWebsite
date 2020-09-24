277862eff4893544307dd054259df8ea3a3243ef
PrastowoAdi <prastowoadinugroho@gmail.com>
2020-09-24 20:01:50 +0700

    Create Navbar and styling

---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Button.css';
+
+const STYLES = ['btn--primary', 'btn--outline']
+
+const SIZEZ = ['btn-medium', 'btn--large', 'btn--mobile', 'btn--wide']
+
+const COLOR = ['primary', 'blue', 'red', 'green']
+
+export const Button = ({ children, type, onClick, buttonStyle, buttonSize,buttonColor}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZEZ.includes(buttonSize) ? buttonSize : SIZEZ[0]
+
+    const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null
+
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
+    )
+}
